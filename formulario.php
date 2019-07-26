e3c56de10a558f1b45dff8a5375eb33c4738e773
João Victor Fernandes Caldas <33259878+joao17victor@users.noreply.github.com>
2019-07-25 20:37:31 -0400

    Add files via upload

---
+++ formulario.php
@@ -0,0 +1,44 @@
+<?php
+
+	if (isset($_POST['nome']) && isset($_POST['email'])) &&
+	!empty($_POST['nome']) == false){
+
+
+}
+		
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<form method="post" >
+		<imput type="text" name="nome" placeholder="informe seu nome:"/>
+		<imput type="email" name="Email" placeholder="informe seu email:"/>
+		<imput type="submit" value="Enviar"/>
+	</form>
+</body>
+</html>
+
+<?php
+    $numero = 1;
+    $nome = 'Joao Victor';
+    
+    && E
+    || OU 
+    ==//IGUAL
+    ===//IDENTICO
+    >//MAIOR
+    <//MENOR
+    =//ATRIBUIÇÃO
+    <=//MENOR IGUAL
+    >=//MAIOR IGUAL
+    * //MULTIPLICAÇÃO
+    /  //DIVISAO
+    + //ADIÇÃO
+    - //SUBTRAÇÃO
+     
+?>
